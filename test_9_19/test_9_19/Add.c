5a1d0fd1e4164fdb939c8b967eec83e0ac327eb3
5baimian <24163339565@qq.com>
2025-06-16 02:46:02 +0800

    本地推送测试

--- test_9_19/test_9_19/Add.c
+++
@@ -1,8 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-int Add(int x, int y)
-{
-	if (x > y)
-		return x;
-	else
-		return y;
-}
--- test_9_19/test_9_19/test.c
+++
@@ -1,9 +0,0 @@
-#define _CRT_SECURE_NO_WARNINGS 1
-
-#include<stdio.h>
-void test()
-{
-	static int a = 1;//a是一个静态的局部变量，
-  	a++;
-	printf("%d\n", a);
-}
