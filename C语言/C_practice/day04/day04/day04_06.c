d2c750a0f8bf9faf269de94d6c4f9026eb198a1a
5baimian <2416339565@qq.com>
2025-06-15 18:07:21 +0800

    完成第六题

--- C语言/C_practice/day04/day04/day04_06.c
+++ C语言/C_practice/day04/day04/day04_06.c
@@ -1,3 +1,64 @@
-/*D06
-��n��Χ��һȦ��˳���źš��ӵ�1���˿�ʼ��������1��3��������
-������3�����˳�Ȧ�ӣ���������µ���ԭ���ĵڼ��ŵ���λ��*/
+﻿/*D06
+有n人围成一圈，顺序排号。从第1个人开始报数（从1到3报数），
+凡报到3的人退出圈子，问最后留下的是原来的第几号的那位。*/
+
+//依次从1到3报数，最后只剩下一个人
+/*
+* 约瑟夫环问题（Josephus Problem）
+* 
+* 有 ​​n​​ 个人围成一圈，编号为 ​​1, 2, ..., n​​。从第一个人开始报数，数到 ​​k​​ 的人被淘汰出局，
+* 然后从下一个人重新开始报数，直到所有人全部出局。求 ​​最后剩下的人的初始编号​​。
+*/
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int josephus(int n, int k);
+//int main()
+//{
+//	int n = 0, k = 3;
+//	printf("请输入一共有n人围成一排\n");
+//	scanf_s("%d", &n);
+//
+//	//for (int i = 0; i < n; i++)
+//	//{
+//	//	printf("%d ", arr[i]);
+//	//}
+//	printf("剩下的人的初始编号为%d\n", josephus(n, k));
+//
+//
+//	return 0;
+//}
+//
+//int josephus(int n, int k)
+//{
+//	int* arr = (int*)malloc(n * sizeof(int));
+//	if (arr == NULL)
+//	{
+//		printf("内存分配失败");
+//		return;
+//	}
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		arr[i] = i + 1;
+//	}
+//	//定义剩余人数remaining，下标Index
+//	int remaining = n, Index = 0;
+//	while (remaining > 1)
+//	{
+//        Index = (Index + k - 1) % remaining;//计算淘汰者的位置
+//
+//		//%remaining是为了防止数组越界,实现环形遍历
+//		for (int j = Index; j < remaining - 1; j++)
+//		{
+//			arr[j] = arr[j + 1];
+//		}
+//		remaining--;
+//	}
+//	//只剩一个人，其下标为0
+//	int last = arr[0];//此时的下标对应值就是数组中的值，而不是arr[0]-1
+//	//因为在while循环中，在去掉淘汰者的位置时会将剩余数组往前移动一位，弥补初始数组下标与对应值之间的差值，因此不需要-1
+//	return last;
+//
+//	free(arr);
+//}
--- C语言/C_practice/day04/day04/day04_07.c
+++ C语言/C_practice/day04/day04/day04_07.c
@@ -1,3 +1,69 @@
 /*D07
 有一字符串，包含n个字符。写一函数，
 将此字符串中从第m个字符开始的全部字符复制成为另一个字符串。*/
+
+/*
+* 创建一个字符串，有n个字符，使用动态分配内存进行创建
+* 动态分配内存存放 m个字符串
+* 将字符串进行复制
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void my_strcpy(char arr1[], char arr2[],int n,int m);
+int main()
+{
+	int n = 0, m = 0;
+	printf("请输入字符串中包含字符的个数n的大小以及 m的大小(n>m)：\n");
+	scanf_s("%d %d", &n,&m);
+
+	// 清除输入缓冲区中的换行符
+	while (getchar() != '\n');//否则无法继续输入字符串
+
+	char* arr1 = (char*)malloc(n * sizeof(char));
+	if (arr1 == NULL)
+	{
+		printf("内存分配出错！");
+		return 1;
+	}
+
+	printf("请输入字符串：\n");
+
+	fgets(arr1, n+1, stdin);
+	arr1[strcspn(arr1, "\n")] = '\0';
+
+
+	char* arr2 = (char*)malloc(m * sizeof(char));
+	if (arr2 == NULL)
+	{
+		printf("内存分配出错！");
+		return 1;
+	}
+
+	if (n > m)
+	{
+		my_strcpy(arr1, arr2, n, m);
+		printf("字符串1为:%s", arr1);
+		printf("字符串2为:%s", arr2);	
+	}
+	else
+	{
+		printf("请重新输入（保证n>m）");
+	}
+
+	free(arr1);
+	free(arr2);
+
+	return 0;
+}
+
+void my_strcpy(char arr1[], char arr2[],int n ,int m)
+{
+	for (int i = 0; i < n - m+1; i++)
+	{
+		arr2[i] = arr1[m + i];
+	}
+	arr2[n-m] = '\0'; // 添加字符串结束符
+}
